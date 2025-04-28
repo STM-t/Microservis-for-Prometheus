f4438d02e57d9020e3ba5f82ffd0915bab626a9a
STM-t <salahov.timurok19@gmail.com>
2025-04-28 14:22:54 +0300

    Delete docker.sh

--- docker.sh
+++
@@ -1,5 +0,0 @@
-dnf install -y dnf-plugins-core
-    dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    dnf install -y docker-ce docker-ce-cli containerd.io
-    systemctl enable --now docker
-    usermod -aG docker $whoami
