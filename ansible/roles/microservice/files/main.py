c08f86519432eedd75ead5249b8bda90f501ab70
STM-t <salahov.timurok19@gmail.com>
2025-09-10 17:31:23 +0300

    Update main.py

--- ansible/roles/microservice/files/main.py
+++ ansible/roles/microservice/files/main.py
@@ -2,31 +2,20 @@ from prometheus_client import start_http_server, REGISTRY, Info, Gauge
 import os
 import time
 import psutil
- 
 
+
+# Оснонвые метрики для мониторинга
 HOST_TYPE = Info('host_type', 'Тип хоста сервера', ['t ype'])
 CPU_USAGE = Gauge('cpu_usage_percent', 'Использование CPU в процентах')
 MEMORY_USAGE = Gauge('memory_usage_percent', 'Использование RAM в процентах')
 
 
-# REGISTRY.unregister(REGISTRY._names_to_collectors['python_gc_objects_collected_total'])
-# REGISTRY.unregister(REGISTRY._names_to_collectors['python_info'])
-
-
-# Вместо старой конструкции ⬆, добавлена функция безопасного удаления лишних питоновских метрик ⬇
-
-
 def safe_unregister(metric_name):
-    """Безопасное удаление избыточных метрик"""
+    """Безопасное удаление лишних питоновских метрик"""
     collector = REGISTRY._names_to_collectors.get(metric_name)
     if collector:
         REGISTRY.unregister(collector)
 
-
-safe_unregister('python_gc_objects_collected_total')
-safe_unregister('python_info')
-   
-   
 
 def get_host_type():
     if os.path.exists('/.dockerenv') or os.environ.get('KUBERNETES_SERVICE_HOST'):
@@ -52,14 +41,20 @@ def get_host_type():
     return 'physical_server'
 
 
+# Обновление статичных метрик - типа хоста
 def setup_static_metrics():
     host_type = get_host_type()
     HOST_TYPE.labels(type=host_type)
 
 
+# Обновление динамических метрик - CPU и RAM
 def update_dynamic_metrics():
     CPU_USAGE.set(psutil.cpu_percent())
     MEMORY_USAGE.set(psutil.virtual_memory().percent)
+
+
+safe_unregister('python_gc_objects_collected_total')
+safe_unregister('python_info')
 
 
 if __name__ == '__main__':
